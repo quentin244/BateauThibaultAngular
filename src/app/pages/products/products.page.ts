c5909dab144152d853ed55bc3f4af698a9a55f5d
quentin <quentin.joubert28@gmail.com>
2020-10-22 11:49:30 +0200

    resolve image

--- src/app/pages/products/products.page.ts
+++ src/app/pages/products/products.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { GetDataService } from '../../services/getData.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,7 +10,6 @@ export class ProductsPage implements OnInit {
   public category = [{"name":"Poisson", "id":0},{"name":"Coquillage", "id":1},{"name":"Crustace", "id":2},{"name":"Promotion", "id":3}];
 
   constructor(
-    private getDataService: GetDataService,
     private router: Router
     ) {
   }
